fa50362496d82cabd41716000afe5b128735519f
ervinMamutov <ervin.news@gmail.com>
2023-05-24 12:54:40 +0200

    addede --boolean.js

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
@@ -6,3 +6,13 @@ TODO: create a few variables(null):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+let value; // declared
+
+value = null; // initialized
+
+console.log('value = ' + value); // read value
+
+value = [1, 2, 3, 4]; // reassigned value
+
+console.log('value[3] = ' + value[3]); // read value
