1230b3ba635be8896c2fc8f7bbf06cda1fca8bb6
ervinMamutov <ervin.news@gmail.com>
2023-05-24 11:24:39 +0200

    add 2-number

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/2-numbers.js
@@ -6,3 +6,18 @@ TODO: create a few variables(numbers):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+
+let sum; // declared
+
+let a = 198987; // declared & initialized
+
+let b = 32324; // declared & initialized
+
+sum = a + b; // read a, read b, initialized
+
+console.log(`sum: ${a} + ${b}  = ${sum}`); // read a, read b, read sum
+
+a = sum + b; // read sum, read b, reassigned a
+
+console.log(`new a : ${sum} + ${b} = ${a}`); // read sum, read b, read a
