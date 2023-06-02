fae80934d2fa826c4ce9ca980fc682be8a52667f
ervinMamutov <ervin.news@gmail.com>
2023-06-02 10:39:03 +0200

    finish 15:11 (mirror & pyramid)

--- 2-just-enough-javascript/15-for-of-loops/11-dom-ui/exercises/pyramid/app.js
+++ 2-just-enough-javascript/15-for-of-loops/11-dom-ui/exercises/pyramid/app.js
@@ -4,13 +4,23 @@ import {
   displayString,
 } from '../../../../../lib/dom-io/index.js';
 
-whenFormDataChanges('___', () => {
-  // debugger;
+whenFormDataChanges('input', () => {
+  debugger;
   console.log('--- form data changed ---');
 
   // --- read the user's input ---
 
+  let input = readString('pyramid-bricks');
+  let textPyramid = '\n';
+  let midalPyramid = '';
+
   // --- build a pyramid ---
 
+  for (const character of input) {
+    midalPyramid += character;
+    textPyramid += midalPyramid + '\n';
+  }
+
   // --- display the pyramid ---
+  displayString('out', textPyramid);
 });
