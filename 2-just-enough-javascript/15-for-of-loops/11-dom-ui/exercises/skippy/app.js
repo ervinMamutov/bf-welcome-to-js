359127859acf5097a59ec948dc9aad0c2e00f209
ervinMamutov <ervin.news@gmail.com>
2023-06-02 15:57:40 +0200

    finish 15:11-skkipy

--- 2-just-enough-javascript/15-for-of-loops/11-dom-ui/exercises/skippy/app.js
+++ 2-just-enough-javascript/15-for-of-loops/11-dom-ui/exercises/skippy/app.js
@@ -6,12 +6,23 @@ import {
 } from '../../../../../lib/dom-io/index.js';
 
 whenFormDataChanges('skippable', () => {
-  // debugger;
+  debugger;
   console.log('--- form data changed ---');
 
   // --- read the user's input ---
+  let inputValue = readString('phrase');
+  let skipValue = readNumber('skip-size');
+  let resultValue = '';
 
   // --- skip characters ---
+  for (let i = 0; i < inputValue.length; i++) {
+    if (i % skipValue !== 0) {
+      resultValue += inputValue[i];
+    } else {
+      continue;     
+    }
+  }
 
   // --- display the result ---
+  displayString('skipped', resultValue);
 });
