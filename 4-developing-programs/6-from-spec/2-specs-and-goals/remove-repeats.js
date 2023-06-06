1ceda3b61e5942c91ee30275ac21a1e82b493650
ervinMamutov <ervin.news@gmail.com>
2023-06-06 16:59:09 +0200

    finish 4-developing-programs/6-from-spec

--- 4-developing-programs/6-from-spec/2-specs-and-goals/remove-repeats.js
+++ 4-developing-programs/6-from-spec/2-specs-and-goals/remove-repeats.js
@@ -31,15 +31,45 @@
       '.@@..@@.'    ->  '".@@..@@." -> ".@"'
 
 */
-
 /* --- gather user input --- */
 
-/* --- check if the input has any repeated characters --- */
+let message = null;
+let resultRepeat = 0;
 
-//  store the string with no repeats for later
+while (!message) {
+  message = prompt('enter text containing any repeating characters');
+
+  if (message === null) {
+    alert('Please, dont press "cansel"');
+    continue;
+  }
+
+  if (message === '') {
+    alert("Please don't enter empty string");
+    continue;
+  }
+}
+
+/* --- check if the input has any repeated characters --- */
+let uniqueCharacter = '';
+
+for (const character of message) {
+  if (!uniqueCharacter.includes(character)) {
+    uniqueCharacter += character;
+  } else {
+    resultRepeat++;
+  }
+  
+}
+
+//  store the search results as a boolean
 
 /* --- create a message for the user --- */
 
-//  you can compare the input to the string without repeats
-
 /* --- display the message to the user --- */
+
+if (resultRepeat) {
+  alert(`${message} has at ${resultRepeat} repeat. \n Unique characters ${uniqueCharacter}`);
+  } else {
+  alert(`${message} has no repeats`);
+}
--- 4-developing-programs/6-from-spec/3-only-specs/has-a-dog.js
+++ 4-developing-programs/6-from-spec/3-only-specs/has-a-dog.js
@@ -1,5 +1,3 @@
-// #todo
-
 'use strict';
 
 /* Has a Dog
@@ -25,3 +23,59 @@
       'GOdoGO'      ->  'at least one dog'
       'A fast dog.' ->  'at least one dog'
 */
+
+let message = null;
+
+const lowetCaseWord = 'dog';
+const upperCaseWord = 'DOG';
+
+let resulteMessage = '';
+
+// The function gets two words and text as parametrs
+function newWord(charOne, charTwo, text) {
+  let resulteText = '';
+  let intermediateResult = text;
+  let remainder = '';
+  let findCharOne = 0;
+  let findCharTwo = 0;
+
+
+  for (const character of text) {
+    let position = intermediateResult.indexOf(character);
+    if (character === charOne.slice(0, 1)) {
+      resulteText += intermediateResult.slice(0, position) + charOne;
+      intermediateResult = intermediateResult.slice(position + 1);
+      findCharOne++;
+    } else if (character === charTwo.slice(0, 1)) {
+      resulteText += intermediateResult.slice(0, position) + charTwo;
+      intermediateResult = intermediateResult.slice(position + 1);
+      findCharTwo++;
+    } else {
+      remainder = intermediateResult;
+    }
+  }
+
+  // The function returns the new tex,
+  // in which the characters on the words 
+  // have been replaced whith the specified words
+  const finalText = resulteText + remainder;
+  return alert( ` We gets word ${text} and find \n ${findCharOne} little ${charOne} \n and \n ${findCharTwo} big ${charTwo}. \n\n  and got a new word as a result ${finalText}` );
+}
+
+while (!message) {
+  message = prompt('Enter the text and I maybe will change it');
+
+  if (message === null) {
+    alert('Don\'t you want to see a new text. \n Don\'t press "cancel" ');
+    continue;
+  }
+
+  if (message === '') {
+    alert("Don't you want to see a new text. \n Don't enter empty string ");
+    continue;
+  }
+}
+
+resulteMessage = newWord(lowetCaseWord, upperCaseWord, message);
+
+
