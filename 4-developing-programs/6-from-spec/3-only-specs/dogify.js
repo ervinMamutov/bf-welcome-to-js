2a6c610f506c9dd955c20f0c3006b2e3d72fa626
ervinMamutov <ervin.news@gmail.com>
2023-06-06 14:51:19 +0200

    finish 4-developing-programs/6-from-spec : 1, 2 (3 need will finish)

--- 4-developing-programs/6-from-spec/3-only-specs/dogify.js
+++ 4-developing-programs/6-from-spec/3-only-specs/dogify.js
@@ -35,3 +35,79 @@
       'Do dump'     ->  'DOGo dogump'
       'dim mooD'    -> 'dogim mooDOG'
 */
+
+let message = null;
+
+const lowetCaseWord = 'dog';
+const upperCaseWord = 'DOG';
+
+let resulteMessage = '';
+
+function position(char, text) {
+  let resulte = [];
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === char) {
+      resulte.push(i);
+    } else {
+      continue;
+    }
+  }
+  return resulte;
+}
+
+while (!message) {
+  message = prompt('Enter the text and I maybe will change it');
+
+  if (message === null) {
+    alert('Don\'t you want to see a new text. \n Don\'t press "cancel" ');
+    continue;
+  }
+
+  if (message === '') {
+    alert("Don't you want to see a new text. \n Don't enter empty string ");
+    continue;
+  }
+}
+
+const lowetCasePosition = position(lowetCaseWord.slice(0, 1), message);
+
+
+let intermediateResult = message;
+let voluePosition = 0;
+let isDogInside = true;
+
+while (isDogInside) {
+  if (lowetCasePosition.length != 0) {
+    voluePosition = lowetCasePosition.pop();
+    resulteMessage +=
+      intermediateResult.slice(0, voluePosition) + lowetCaseWord;
+    intermediateResult = intermediateResult.slice(voluePosition + 1);
+    isDogInside = true;
+  } else {
+    resulteMessage += intermediateResult;
+    isDogInside = false;
+  }
+}
+
+const upperCasePosition = position(upperCaseWord.slice(0, 1), resulteMessage);
+intermediateResult = resulteMessage;
+voluePosition = 0;
+isDogInside = true;
+
+while (isDogInside) {
+  if (upperCasePosition.length != 0) {
+    voluePosition = upperCasePosition.pop();
+    
+    resulteMessage +=
+      intermediateResult.slice(0, voluePosition) + upperCaseWord;
+    intermediateResult = intermediateResult.slice(voluePosition + 1);
+    isDogInside = true;
+  } else {
+    resulteMessage += intermediateResult;
+    isDogInside = false;
+  }
+}
+
+alert(message + ' ' + lowetCasePosition + ' ' + resulteMessage);
+//alert(message + upperCasePosition);
