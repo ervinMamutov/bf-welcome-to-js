ce48f914382e530be717ace3cff56144fd67d481
ervinMamutov <ervin.news@gmail.com>
2023-06-07 10:15:15 +0200

    finish 4-developing-programs/7-reverse-engineering/exercises

--- 4-developing-programs/7-reverse-engineering/exercises/mystery-1.js
+++ 4-developing-programs/7-reverse-engineering/exercises/mystery-1.js
@@ -1,11 +1,44 @@
 'use strict';
 
-/* title
+/* title palindrome
 
-  description
+  description  enter a palindrome, this program will reverse it
 
-  user story + acceptance criteria
+  user story + acceptance criteria 
+  look up the word-palindrome
 
   test cases
 
+  repaper -> repaper
+  civic -> civic
 */
+
+let palindrome = null;
+let isHerPoliandrome = false;
+
+while (!isHerPoliandrome) {
+  palindrome = prompt('enter a palindrome, this program will reverse it');
+
+  if (palindrome === null) {
+    alert('nope, enter something');
+    continue;
+  }
+
+  if (palindrome === '') {
+    alert('nope, enter something');
+    continue;
+  }
+
+  let palindromeReverse = palindrome.split('').reverse().join('');
+
+  if (palindrome != palindromeReverse) {
+    alert('your input is not a palindrome, try again');
+    isHerPoliandrome = false;
+    continue;
+  }
+
+  if (palindrome === palindromeReverse) {
+    alert('her is your revesed palindrome');
+    break;
+  }
+}
--- 4-developing-programs/7-reverse-engineering/exercises/mystery-3.js
+++ 4-developing-programs/7-reverse-engineering/exercises/mystery-3.js
@@ -1,11 +1,76 @@
 'use strict';
 
-/* title
+/* title: long phrase
 
-  description
+  description: search for a word according to given parameters
 
-  user story + acceptance criteria
+  user story + acceptance criteria: The user enters text. Then enter the characters to make sure it does or does not contain text.
+
 
   test cases
 
+  qwertyazerty -> qw -> 'qwertyazerty" does include "qw"'
 */
+
+
+let longPhrase = null;
+let searchQuery = null;
+let checking;
+let isLongPhrase = false;
+
+while(!isLongPhrase) {
+  longPhrase = prompt ('enter a long phrase (at least 10 characters)');
+
+  if (longPhrase === null) {
+    continue;
+  };
+
+  if (longPhrase === '') {
+    alert('not long enough');
+    continue;
+  }
+
+  if (longPhrase.length < 10) {
+    alert('not long enough');
+    isLongPhrase = false;
+    continue;
+  }
+
+  if (longPhrase.length >= 10) {
+    alert(`is this phrase correct: ${longPhrase}`);
+  }
+
+  if (confirm(`is this phrase correct: ${longPhrase}`)) {
+    break;
+  } else {
+    continue;
+  }
+};
+
+while (!isLongPhrase) {
+  searchQuery = prompt('enter a search query, this program will check if it exists in your phrase');
+
+  if (searchQuery === null) {
+    continue;
+  };
+
+  if (confirm(`is this query correct: ${searchQuery}`)) {
+    break;
+  } else {
+    isLongPhrase = false;
+    continue;
+  }
+};
+
+if (longPhrase.includes(searchQuery)) {
+  alert (`${longPhrase} does include ${searchQuery}`);
+} else {
+  alert (`${longPhrase} does not include ${searchQuery}`);
+};
+
+
+
+
+
+
+
